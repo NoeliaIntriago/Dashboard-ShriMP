150a21d814c297eb116c994a36e75b8911b1c037
Noelia Intriago <noinsanc@espol.edu.ec>
2024-01-11 09:20:09 -0500

    feat: files for dashboard deploy

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Dashboard-ShriMP
+
+# Setup
+## venv
+Create virtual environment (if created skip this step)
+
+```
+    py -m venv .venv
+```
+
+Activate virtual environment
+
+```
+    .venv\Scripts\activate.bat
+```
+
+Install required packages for the project inside venv:
+
+```
+    pip install -r requirements.txt 
+```
+
+## App
+
+```
+    streamlit run dashboard.py
+```
+
---
+++ queries.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import traceback
+
+## GET ALL CLIENTS
+def get_clients(connection):
+    try: 
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM cliente')
+        data = [row[2] for row in cursor.fetchall()]
+        cursor.close()
+
+        return data, 200
+    except Exception as e:
+        print(traceback.format_exc())
+        return 'Something went wrong', 500
+    
+## GET HISTORY  
+def get_historic(connection, params):
+    try:
+        cursor = connection.cursor()
+        base_query = '''
+            SELECT 
+                des_cliente as Cliente,
+                des_sku as Producto,
+                familia as Familia,
+                fecha_emision as Fecha,
+                grupo_linea as Etapa,
+                toneladas as Toneladas
+            FROM venta 
+            INNER JOIN cliente ON venta.id_cliente = cliente.id_cliente 
+            INNER JOIN producto ON venta.id_producto = producto.id_producto
+        '''
+        start_date = datetime(int(params['year']), int(params['month']), 1).strftime('%Y-%m-%d')
+
+        if (int(params['month']) == 12):
+            end_date = datetime(int(params['year']) + 1, 1, 1).strftime('%Y-%m-%d')
+        else:
+            end_date = datetime(int(params['year']), int(params['month']) + 1, 1).strftime('%Y-%m-%d')
+
+        conditions = []
+
+        conditions.append(f"venta.fecha_emision BETWEEN '{start_date}' AND '{end_date}'")
+
+        if params['stage'] is not None:
+            conditions.append(f"producto.grupo_linea = '{params['stage']}'")
+
+        if params['client'] is not None:
+            conditions.append(f"cliente.des_cliente = '{params['client']}'")
+
+        final_query = f"{base_query} WHERE {' AND '.join(conditions)}"
+
+        cursor.execute(final_query)
+        
+        data = cursor.fetchall()
+        columns = [i[0] for i in cursor.description]
+        cursor.close()
+
+        formatted_data = [
+            {columns[i]: row[i] for i in range(len(columns))} 
+            for row in data
+        ]
+
+        return formatted_data, 200
+    except Exception as e:
+        print(traceback.format_exc())
+        return 'Something went wrong', 500
+
+# UPLOAD DATA FUNCTION
+def upload_file(connection, file):
+    print(file)
+    # response = requests.post('http://127.0.0.1:5000/api/upload-file', files={'file': file})
+    # output = response.json()
+    # return output
